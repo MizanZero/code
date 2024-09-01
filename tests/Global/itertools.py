c728c3a1bf81ef274baaca9e1cced1e91f4367c8
MizanZero <mizan.zafar2016@gmail.com>
2024-09-01 15:57:20 +0530

    2nd commit

--- tests/Global/itertools.py
+++ tests/Global/itertools.py
@@ -8,9 +8,19 @@ def cartesian_product(rep = 1):
 
 print(cartesian_product(2))
 
-from itertools import permutations
+from itertools import permutations 
 def set_permutation(n=2):
     sp = list(permutations(A, n)) # n is the number of elements in the permuted sets
     return sp
 
 print(set_permutation())
+
+from itertools import combinations, combinations_with_replacement
+def set_combination(n=2):
+    sc = list(combinations(A, n))
+    return sc
+
+def set_combination_replacement(n=2): # repetitions are allowed e.g.: (1,1)
+    scr = list(combinations_with_replacement(A,n))
+    return scr
+
