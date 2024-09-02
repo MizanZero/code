adea985382be845b7851fae3c0453f8f7f5dfe20
MizanZero <mizan.zafar2016@gmail.com>
2024-09-02 14:15:44 +0530

    2/9/24 2:16 Afternoon

--- tests/Global/itertools.py
+++ tests/Global/itertools.py
@@ -6,14 +6,14 @@ def cartesian_product(rep = 1):
     cp = product(A,B, repeat=rep) # repeat permutes all the sets among themselves said number of times.
     return ("List of cartesian product of "+str(A)+" "+str(B)+": "+str(list(cp)))
 
-print(cartesian_product(2))
+#print(cartesian_product(2))
 
 from itertools import permutations 
 def set_permutation(n=2):
     sp = list(permutations(A, n)) # n is the number of elements in the permuted sets
     return sp
 
-print(set_permutation())
+#print(set_permutation())
 
 from itertools import combinations, combinations_with_replacement
 def set_combination(n=2):
@@ -27,8 +27,7 @@ def set_combination_replacement(n=2): # repetitions are allowed e.g.: (1,1)
 from itertools import accumulate
 import operator
 def cumulate(option):
-    acc = list(accumulate(A, func=eval(option))) #result of every step is dragged along and
-                                           #func is applied in each step
+    acc = list(accumulate(A, func=eval(option))) #result of every step is dragged along and func is applied to each element with the previous result
     return acc
+print(cumulate("operator.sub"))
 
-print(cumulate("operator.mul"))
