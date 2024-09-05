fc460043136328acb98c5ee2e72b00fdd8b3d19a
MizanZero <mizan.zafar2016@gmail.com>
2024-09-05 12:58:47 +0530

    previous one had guessers untracked fsr

---
+++ tests/Global/num_guesser-alternate.py
@@ -0,0 +1,33 @@
+import random
+
+inp_is_valid = False
+inp_is_correct = False
+
+print("\n\n\n")
+
+
+print("Guess a random integer from -10 to 10")
+
+guess = ""
+ans = random.randint(-10,10)
+#print (ans) #remove first hash only to get the right answer printed at the beginning for debug
+
+while inp_is_valid == False or inp_is_correct == False:
+    guess = input("Enter guess: ")
+    if guess.isnumeric() or guess.replace("-","").isnumeric():
+        inp_is_valid = True
+
+        if int(guess) in range(-10,11):
+
+            if int(guess)>ans:
+                print("Your guess was higher than the answer, try again")
+            elif int(guess)<ans:
+                print("Your guess was lower than the answer, try again")
+            else:
+                print(f"You guessed it. It was {ans}")
+                inp_is_correct = True
+        else:
+            print("Only enter an integer from -10 to 10")
+
+    else:
+        print("Only enter an integer from -10 to 10")
---
+++ tests/Global/num_guesser.py
@@ -0,0 +1,59 @@
+import random
+
+
+print("\n\n\n")
+print("Guess a random integer from -10 to 10")
+
+
+ans = random.randint(-10,10)
+print (ans) #remove first hash only to get the right answer printed at the beginning for debug
+
+
+def getinput():
+    guess = input("Enter guess: ")
+    return guess
+
+
+def checkvalid(guess):
+    if guess.isnumeric() or (guess[0]=="-" and guess[1:].isnumeric()): #first condition to check for positive and second for negative
+        if int(guess) in range(-10,11):
+            valid = True
+        else:
+            print("-------------------------------------\nOnly enter an integer from -10 to 10\n-------------------------------------")
+            valid = False
+    else:
+        print("-------------------------------------\nOnly enter an integer from -10 to 10\n-------------------------------------")
+        valid = False
+    return valid
+
+
+def checkans(valid, guess, ans):
+    if valid == True:
+        if int(guess) > ans:
+            print("---------------------------\nYour guess was higher than the answer, try again\n---------------------------")
+            ansisright = False
+        elif int(guess) < ans:
+            print("---------------------------\nYour guess was lower than the answer, try again\n---------------------------")
+            ansisright = False
+        elif int(guess)==ans:
+            ansisright = True
+        else:
+            ansisright = False
+    else:
+        ansisright = False
+    return ansisright
+
+
+valid = False
+ansisright = False
+
+
+while valid == False or ansisright == False:
+    guess = getinput()
+    valid = checkvalid(guess)
+    ansisright = checkans(valid, guess, ans)
+
+
+print(f"\n---------------------------\nYou guessed it! It was {ans}\n---------------------------\n")
+
+
