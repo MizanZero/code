cbf055a2ee1ee1f8d4f77424923573a8f75ba779
MizanZero <mizan.zafar2016@gmail.com>
2024-09-21 22:13:13 +0530

    list sort tested for strings

--- tests/Global/list tests.py
+++ tests/Global/list tests.py
@@ -3,6 +3,14 @@ list1 = [x for x in range(1, 51)]
 #print (list1[0:])
 
 list2 = [2,2,2,2,2]
-print(list2.index(2))
-print("😋")
+#print(list2.index(2))
+#print("😋")
 
+list3 = [1,2,"a","A",9,0.1]
+list4 = ["a","A","b","B"]
+list5 = [1,3,2]
+list5.sort()
+list4.sort()
+print(f"{list4}")
+#print(list4.sort())
+#print(list3.sort())
