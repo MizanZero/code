9be26c7d99daa888e9f8e9dedf555e6b39cb06f1
MizanZero <mizan.zafar2016@gmail.com>
2024-10-30 20:53:11 +0530

    hand cricket object oriented

---
+++ tests/Global/hand_crick.py
@@ -0,0 +1,25 @@
+import random
+
+oddList = ["odd","od","dd","do","o","d"]
+eveList = ["eve",'ev','ve','e']
+
+def inp(inpMsg):
+    inp=input(inpMsg)
+    return inp,inp.isnumeric()
+
+def oddOrEve():
+    oddOrEve=inp("Enter Odd or Even: ")[0].lower()
+    if oddOrEve in eveList:
+        return 'odd'
+    elif oddOrEve in oddList:
+        return 'eve'
+
+def match():
+    usrThrow,=inp("Throw a number: ")[0,1]
+    if 
+
+
+
+
+
+
