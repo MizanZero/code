3d9fb812777e872a5aee5317be01adaa26cae88e
MizanZero <mizan.zafar2016@gmail.com>
2024-10-31 22:22:46 +0530

    continuing, no idea what happened, had to do a pull before pushing the last one

--- tests/Global/hand_crick.py
+++ tests/Global/hand_crick.py
@@ -1,41 +1,67 @@
 import random 
 
 oddList = ["odd","od","dd","do","o","d"] 
-eveList = ["even","ven","eve",'ev','ve','e'] 
-battingList=[0,0,0,0,1] 
+eveList = ["even","eve","ven","eve",'ev','ve','e'] 
+batList=['batting','bat']
+bowlList=['bowl','ball','bal']
+biasList=[0,0,0,0,1] 
 
 
 def inp(inpMsg):
     inp=input(inpMsg) 
-    return inp,inp.isnumeric() 
+    return inp,inp.isnumeric() #return user enter and bool for numeric
 
 
-def outOrNot(p,c):
+def isOut(p,c):
     return False if p==c else True 
 
 
-def oddOrEve():
+def controlledInp(l1,l2,r1,r2,takeInt,inpMsg,warning=""):
+    if takeInt:
+        inp = inp(inpMsg) 
+        while True:
+            inp=inp(inpMsg) 
+            if inp[1]:
+                inp = int(inp) 
+            else:
+                print(warning)
     while True:
-        oddOrEve=inp("Enter Odd or Even: ")[0].lower() 
-        if oddOrEve in eveList:
-            return 'eve'
-        elif oddOrEve in oddList:
-            return 'odd'
+        inp=input(inpMsg).lower() 
+        if inp in l1:
+            return r1,inp.isnumeric() #return desired return val,bool for numeric
+        elif inp in l2:
+            return r2,inp.isnumeric() #same 
+        else:
+            print(warning)
+
+
+
 
 
 def throw():
     [usrThrow,usrNum]=inp("Throw a number: ")[0:2] 
-    return usrThrow,usrNum,random.randint(1,10) 
+    return usrThrow,usrNum,random.randint(1,10) #return usr value, boolean for numeric, and a random from 1 to 10
 
 
-
-if oddOrEve() == 'eve':
+oddOrEve = controlledInp(eveList,oddList,'eve','odd',False,"Enter Odd or Eve: ") 
+if oddOrEve == 'eve':
     oddOrEve='eve'
 else:
     oddOrEve='odd'
 
 
-oddEve=inp("Enter a number for Odd or Eve: ")
+
+oddEve=controlledInp(oddList,eveList,'odd','eve',True,"Enter a number for Odd or Eve(max: 10): ",'Only enter odd or eve(max: 10)')
+print (oddEve) #debug statement 
+
 if oddEve[1]:
-    oddEve = int(oddEve[0])
-    if (throw(2)+oddEve)%2 == 0 and :
+    if int(oddEve[1]) in range(1,11):
+        oddEve = int(oddEve[0])
+        com_oddOrEve=random.randint(1,10)
+        print (com_oddOrEve,oddEve)
+        if (com_oddOrEve+oddEve)%2==0 and oddEve=='eve':
+            batOrBowl=inp('You won, enter Bat or Ball: ')
+        elif (com_oddOrEve+oddEve)%2==1 and oddEve=='odd':
+            batOrBowl=inp('You won, enter Bat or Ball: ')
+
+
