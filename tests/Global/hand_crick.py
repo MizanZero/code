9743920259b2e584de6c5fe943804dd48f6d654b
MizanZero <mizan.zafar2016@gmail.com>
2024-11-03 22:37:00 +0530

    almost

--- tests/Global/hand_crick.py
+++ tests/Global/hand_crick.py
@@ -29,10 +29,10 @@ def inp(inpMsg,dataType='',warning=""): #return integer if required else return 
 def outOrNot(p,c): #invalid for -1 invalid2 for any other invalid, True for out, False for not out
     if -1 in [p,c]:
         return 'invalid'
-    elif p!=c:
-        return False
-    elif p==c:
+    elif p!=c: 
         return True
+    elif p==c:
+        return False
     else:
         return 'invalid2'
 
@@ -50,24 +50,31 @@ def decide(l1,l2,r1,r2,dataType,inpMsg,warning=""): #return input, r1
 
 
 def throw(roleMsg):
-    print(roleMsg)
     usrThrow=decide(range(1,11),range(1,11),'','','int',"Throw a number: ")[0] #take an integer input from 1 to 10
     return usrThrow,random.randint(1,10) #return usr value, and a random from 1 to 10
 
 
 
 def switchRole(batting):
+    global usrRole
+    global comRole
     if batting=='usr':
         batting='com'
+        usrRole='bowl'
+        comRole='bat'
     else:
         batting='usr'
+        usrRole='bat'
+        comRole='bowl'
+
+
 
 
 oddOrEve = decide(eveList,oddList,'eve','odd','',"Enter Odd or Eve: ","Only enter Odd or Eve!")[1]  #stores odd or eve 
 #print (oddOrEve) #debug statement
 
 
-oddEveVal=decide(range(1,10,2),range(2,10,2),'odd','eve','int',"Enter a number for Odd or Eve(max: 10): ","Only enter odd or eve(max: 10)")
+oddEveVal=decide(range(1,10,2),range(2,10,2),'odd','eve','int',"Enter a number for Odd or Eve(max: 10): ","Only enter a natural number(max: 10)")
 oddEveVal=oddEveVal[0] 
 #print (oddEveVal) #debug statement 
 
@@ -86,16 +93,18 @@ if usrWillChoose:
     batting=decide(batList,bowlList,'usr','com','notInt',"Batting or Bowling: ","Enter a valid choice!\n")[1] #batting takes usr or com
 else:
     batting=random.choice(['com' for x in biasList if x==0]+['usr']) #computer chooses
-    if batting == 'com':
-        comRole='bat'
-        usrRole = 'bowl'
-    else:
-        comRole='bowl'
-        usrRole='bat'
-    print("Coumputer chose to",comRole)
 
 
-#defining user and computer roles
+if batting == 'com':
+    comRole='bat'
+    usrRole = 'bowl'
+else:
+    comRole='bowl'
+    usrRole='bat'
+print("Coumputer chose to",comRole)
+
+
+#defining user and computer roles 
 if usrRole=='bat':
     roleMsg="You are batting"
 else:
@@ -111,13 +120,19 @@ def declareOut(batting):
 
 thrown={'usr':0,'com':0}  #stores number thrown 
 
+totalInnings = 2
 
 def easyMatch():
-    for inning in range(2):
+    isNotOut=True 
+    for inning in range(1,totalInnings+1): 
         while isNotOut:
-            thrown['usr'],thrown['com']=throw(roleMsg) 
+            print('Inning:',inning) 
+            print('Total innnigs:',totalInnings)
+            print(roleMsg)
             score[batting+'Score']+=thrown[batting] 
-            
+            thrown['usr'],thrown['com']=throw(roleMsg) 
+            isNotOut=outOrNot(thrown['usr'],thrown['com']) 
+        switchRole(batting) 
 
 
 easyMatch()
@@ -130,3 +145,5 @@ def declareResult():
 
     else:
         print("Close Game! It was a tie!",score['comScore'])
+
+declareResult()
