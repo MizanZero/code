dac25d461ef75629b8ec8ac33518e9124e5e9a14
MizanZero <mizan.zafar2016@gmail.com>
2024-11-03 16:47:30 +0530

    added ydm calc to global and fixed most of hand crick

--- tests/Global/hand_crick.py
+++ tests/Global/hand_crick.py
@@ -1,10 +1,12 @@
-import random 
+import random
+
+print()
 
 oddList = ["odd","od","dd","do","o","d"] 
 eveList = ["even","eve","ven","eve",'ev','ve','e'] 
-batList=['batting','bat']
-bowlList=['bowl','ball','bal']
-biasList=['bat','bat','bat','bat','bowl'] 
+batList = ['batting','bat','at','att','atting','ting','tting']
+bowlList= ['bowl','ball','bal','owl','all','al']
+biasList= [0,0,0,0,1] 
 batScore=0
 comScore=0
 usrScore=0
@@ -12,63 +14,66 @@ score = {'usrScore':0, 'comScore':0}
 
 
 
-def inp(inpMsg):
+def inp(inpMsg,dataType='',warning=""): #return integer if required else return input as it is
     inp=input(inpMsg) 
-    return inp,inp.isnumeric() #return user enter and bool for numeric
+    if dataType!='int':
+        return inp
+    elif dataType=='int' and not inp.isnumeric():
+        print()
+        print(warning)
+        inp = input(inpMsg)
+    elif dataType=='int' and inp.isnumeric():
+        return int(inp)
 
 
-def outOrNot(p,c):
-    return True if p==c else False 
-
-
-def controlledInp(l1,l2,r1,r2,dataType,inpMsg,warning=""):
-    
-    if dataType=='int':
-        while True:
-            takeInp=inp(inpMsg)
-            takeInp=list(takeInp) #takes input
-            if takeInp[1]: 
-                takeInp[0] = int(takeInp[0]) 
-                return takeInp[0]
-            else:
-                print(warning,"first loop in contrInp") 
+def outOrNot(p,c): #invalid for -1 invalid2 for any other invalid, True for out, False for not out
+    if -1 in [p,c]:
+        return 'invalid'
+    elif p!=c:
+        return False
+    elif p==c:
+        return True
     else:
-        takeInp=inp(inpMsg)
-        takeInp=list(takeInp) #takes input
+        return 'invalid2'
 
-        while True:
-            if takeInp[0] in l1:
-                return r1,takeInp[1] #return desired return val,bool for numeric
-            elif takeInp[0] in l2:
-                return r2,takeInp[1] #same 
-            else:
-                print(warning,"second loop in contrInp")
-
+def decide(l1,l2,r1,r2,dataType,inpMsg,warning=""): #return input, r1 
+    while True:
+        takeInp=inp(inpMsg,dataType,warning)
+        if takeInp in l1:
+            return takeInp, r1
+        elif takeInp in l2:
+            return takeInp, r2
+        else:
+            print()
+            print(warning)
 
 
 
 def throw(roleMsg):
     print(roleMsg)
-    usrNum=controlledInp(range(1,11),range(1,11),'','','int',"Throw a number: ") 
-    return usrNum,random.randint(1,10) #return usr value, boolean for numeric, and a random from 1 to 10
+    usrNum=decide(range(1,11),range(1,11),'','','int',"Throw a number: ")[0]
+    return usrNum,random.randint(1,10) #return usr value, and a random from 1 to 10
 
 
 
-def switch():
+def switchRole(batting):
     if batting=='usr':
         batting='com'
     else:
         batting='usr'
 
 
-oddOrEve = controlledInp(eveList,oddList,'eve','odd',False,"Enter Odd or Eve: ") 
+oddOrEve = decide(eveList,oddList,'eve','odd','',"Enter Odd or Eve: ","Only enter Odd or Eve!")[1]  #stores odd or eve 
+print (oddOrEve) #debug statement
 
 
-oddEveVal=controlledInp(range(1,10,2),range(2,10,2),'odd','eve','int',"Enter a number for Odd or Eve(max: 10): ",'Only enter odd or eve(max: 10)')
+oddEveVal=decide(range(1,10,2),range(2,10,2),'odd','eve','int',"Enter a number for Odd or Eve(max: 10): ","Only enter odd or eve(max: 10)")
+oddEveVal=oddEveVal[0] 
 print (oddEveVal) #debug statement 
 
-#check if odd or eve
-checkOddEveVal=oddEveVal+random.randint(1,10) #sum to check for oddeve
+#check if odd or eve 
+comOddEveVal = random.randint(1,10)
+checkOddEveVal=oddEveVal+comOddEveVal #sum to check for oddeve 
 if (checkOddEveVal%2==0 and oddOrEve=='eve') or (checkOddEveVal%2==1 and oddOrEve=='odd'):
     usrWillChoose = True
 else:
@@ -77,9 +82,16 @@ else:
 
 #if usr won oddEve ask for bat or ball
 if usrWillChoose:
-    batting=controlledInp(batList,bowlList,'usr','com','notInt',"Batting or Bowling: ","Enter a valid choice!\n")
+    print(oddEveVal,'+',comOddEveVal,'=',oddEveVal+comOddEveVal,"\nYou can choose")
+    batting=decide(batList,bowlList,'usr','com','notInt',"Batting or Bowling: ","Enter a valid choice!\n")[1] #batting takes usr or com
 else:
-    batting=random.choice(biasList)
+    batting=random.choice(['com' for x in biasList if x==0]+['usr'])
+    if batting == 'com':
+        comRole='bat'
+    else:
+        comRole='bowl'
+    print("Coumputer chose to",comRole)
+
 
 
 isNotOut=True
@@ -87,22 +99,38 @@ teamsBowled = 0
 
 
 
+def declareOut(batting):
+    return "You took a wicket!" if batting == 'com' else "You lost a  wicket!"
+
 
 def easyMatch():
+    global teamsBowled
+    if teamsBowled==2:
+        return score['usrScore'],score['comScore']
     if batting == 'usr':
         roleMsg = "You are batting"
     else:
         roleMsg="You are bowling"
     global batScore
     global isNotOut
-    global teamsBowled
-    while isNotOut and batScore<=50 and teamsBowled!=2:
-        [batThrow,bowlThrow]=throw(roleMsg) 
-        isNotOut=outOrNot(batThrow,bowlThrow)
+    while isNotOut and batScore<=50 and teamsBowled!=2: 
+        [batThrow,bowlThrow] = throw(roleMsg) 
+        isNotOut=outOrNot(batThrow,bowlThrow) 
         batScore+=batThrow 
-    score[batting+'Score'] = batScore
+    print(declareOut(batting))
+    score[batting+'Score'] = batScore 
+    print('Runs: ',batScore)
     teamsBowled = teamsBowled+1
-    return batScore if teamsBowled==2 else easyMatch()
+    switchRole(batting)
+    easyMatch()
 
 easyMatch()
 
+def declareResult():
+    if score['usrScore'] > score['comScore']:
+        print("You won!\n",score['usrScore'],":",score['comScore'])
+    elif score['usrScore'] < score['comScore']:
+        print("You lost!\n",score['usrScore'],":",score['comScore'])
+
+    else:
+        print("Close Game! It was a tie!",score['comScore'])
