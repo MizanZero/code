c7e3c702844debacb4dc9a78f5a8beda50a68368
MizanZero <mizan.zafar2016@gmail.com>
2024-11-16 19:14:54 +0530

    started learning scraping

--- Projects/Legal/Web/scraper.py
+++ Projects/Legal/Web/scraper.py
@@ -1,2 +1,11 @@
-from bs4 import beautifulsoup
+from bs4 import BeautifulSoup
 
+with open("C:/Users/Mizan/Documents/Code/PyPrjct/Projects/Legal/Web/home.html",'r') as scrape_file:
+    content=scrape_file.read()
+    #print(content)
+    soup = BeautifulSoup(content,'lxml')
+    #print(soup)
+    print('\n'*10)
+    #print(soup.prettify())
+    p_tags=soup.find_all('p') 
+    print (x for x in list(p_tags)) 
