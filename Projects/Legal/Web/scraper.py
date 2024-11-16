f7e590ff881404e31d66b49db74a6dfe55099be4
MizanZero <mizan.zafar2016@gmail.com>
2024-11-16 20:10:10 +0530

    trying a complex for loop for tags in a 3D dictionary

--- Projects/Legal/Web/scraper.py
+++ Projects/Legal/Web/scraper.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 
-tags = {} # 'tagName' : soup.find_all('tagName') ----> format 
+tags = {} #'tagName' : soup.find_all('tagName') ----> format 
+tags['div']={'class_':{},'style':{},'id':{}} #div contains a dictionary which contians subdicts of class style and id
+
 
 with open("C:/Users/Mizan/Documents/Code/PyPrjct/Projects/Legal/Web/home.html",'r') as scrape_file:
     content=scrape_file.read()
@@ -12,6 +14,15 @@ with open("C:/Users/Mizan/Documents/Code/PyPrjct/Projects/Legal/Web/home.html",'
     tags['p']=soup.find_all('p') 
     tags['h1'] = soup.find_all('h5') 
 
+
+
+    for x in tags.keys():
+        while type(tags[x])==dict:
+            x=tags[x].keys()
+        tags[x] = soup.find_all(x)
+
+
+
     def printTags(tag):
         tagName=tag
         tag = tags[tag]
@@ -20,6 +31,6 @@ with open("C:/Users/Mizan/Documents/Code/PyPrjct/Projects/Legal/Web/home.html",'
             print()
         print("Number of",tagName,'tags is',len(tag))
 
-    printTags('h1')
+    #printTags('h1')
 
-    tags()
+    printTags
