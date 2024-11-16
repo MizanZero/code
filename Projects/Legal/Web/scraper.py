05d602e520a91a902111c3cd44ed471c2afb4abf
MizanZero <mizan.zafar2016@gmail.com>
2024-11-16 19:44:17 +0530

    starting div tags

--- Projects/Legal/Web/scraper.py
+++ Projects/Legal/Web/scraper.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 
+tags = {} # 'tagName' : soup.find_all('tagName') ----> format 
+
 with open("C:/Users/Mizan/Documents/Code/PyPrjct/Projects/Legal/Web/home.html",'r') as scrape_file:
     content=scrape_file.read()
     #print(content)
@@ -7,5 +9,17 @@ with open("C:/Users/Mizan/Documents/Code/PyPrjct/Projects/Legal/Web/home.html",'
     #print(soup)
     print('\n'*10)
     #print(soup.prettify())
-    p_tags=soup.find_all('p') 
-    print (x for x in list(p_tags)) 
+    tags['p']=soup.find_all('p') 
+    tags['h1'] = soup.find_all('h5') 
+
+    def printTags(tag):
+        tagName=tag
+        tag = tags[tag]
+        for x in list(tag):
+            print(x)
+            print()
+        print("Number of",tagName,'tags is',len(tag))
+
+    printTags('h1')
+
+    tags()
