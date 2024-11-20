6be63965cb566ce3882c0179829c2b1665e31d2d
Mizan <86833227+MizanZero@users.noreply.github.com>
2024-11-20 19:48:06 +0530

    Update tempCodeRunnerFile.py

--- tests/Global/tempCodeRunnerFile.py
+++ tests/Global/tempCodeRunnerFile.py
@@ -1,9 +1,28 @@
 print("Your guess was higher than the answer, try again")
 
 
-for c in k:
-  v=chr(ord(c)**2+1) if ord(c)%2==0 else chr(ord(c)**2-1) 
+import math
 
-for c in v:
-  k=chr(math.sqrt(ord(c)-1)) if math.sqrt(ord(c)-1)%2==0 else chr(math.sqrt(ord(c)+1))
+k=''
 
+while k!='exit()':
+	k=input('Enter: ')
+	v=''
+	r=''
+	
+	for c in k:
+		if ord(c)%2==0:
+			v+=chr(ord(c)**2+1)
+		else:
+			v+=chr(ord(c)**2-1)
+	
+	for x in v:
+		if math.sqrt(ord(x)-1)%2==0:
+			r+=chr(int(math.sqrt(ord(x)-1)))
+		else:
+			r+=chr(int(math.sqrt(ord(x)+1)))
+
+	print(k)
+	print(v)
+	print(r)
+	print()
