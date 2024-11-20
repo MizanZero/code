4c638f94b7bac46688b95c2a587c074ae6e03012
MizanZero <mizan.zafar2016@gmail.com>
2024-11-20 20:06:23 +0530

    completed for today

--- tests/Global/tempCodeRunnerFile.py
+++ tests/Global/tempCodeRunnerFile.py
@@ -8,21 +8,28 @@ k=''
 while k!='exit()':
 	k=input('Enter: ')
 	v=''
-	r=''
 	
-	for c in k:
-		if ord(c)%2==0:
-			v+=chr(ord(c)**2+1)
-		else:
-			v+=chr(ord(c)**2-1)
 	
-	for x in v:
-		if math.sqrt(ord(x)-1)%2==0:
-			r+=chr(int(math.sqrt(ord(x)-1)))
-		else:
-			r+=chr(int(math.sqrt(ord(x)+1)))
+	def enc(k):
+		for c in k:
+			if ord(c)%2==0:
+				v+=chr(ord(c)**2+1)
+			else:
+				v+=chr(ord(c)**2-1)
+		print(v)
+	
+	def dec(v):
+		r=''
+		for x in v:
+			if math.sqrt(ord(x)-1)%2==0:
+				r+=chr(int(math.sqrt(ord(x)-1)))
+			else:
+				r+=chr(int(math.sqrt(ord(x)+1)))
+		print(r)
 
-	print(k)
-	print(v)
-	print(r)
-	print()
+	try:
+		enc(k)
+		dec(v)
+	except:
+		dec(k) 
+
