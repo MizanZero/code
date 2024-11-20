246f1987665a0d0253f8b0c8c8fa30a80241645c
Mizan <86833227+MizanZero@users.noreply.github.com>
2024-11-20 19:08:06 +0530

    tempCodeRunnerFile.py

--- tests/Global/tempCodeRunnerFile.py
+++ tests/Global/tempCodeRunnerFile.py
@@ -1,1 +1,9 @@
 print("Your guess was higher than the answer, try again")
+
+
+for c in k:
+  v=chr(ord(c)**2+1) if ord(c)%2==0 else chr(ord(c)**2-1) 
+
+for c in v:
+  k=chr(math.sqrt(ord(c)-1)) if math.sqrt(ord(c)-1)%2==0 else chr(math.sqrt(ord(c)+1))
+
