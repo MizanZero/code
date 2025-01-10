6f5398c66479b40211b6ea10098d3d29e74f6531
MizanZero <mizan.zafar2016@gmail.com>
2025-01-10 18:36:09 +0530

    enum for role

---
+++ hand cricket/Python/hand cricket.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+
+class role(Enum):
+    BAT=['bat','ba','at', 1]
+    BOWL=['bowl','bo','ow','wl','bow','owl', 2]
+
+
+class player:
+    def __init__(self, name:str, role:str, score:int=0):
+        assert role in ['bat','bowl'], "Role should be 'bat' or 'bowl'"
+        assert score>=0, "Score should greater than or equal to 0"
+
+        self.name=name; self.role=role; self.score=score
+
+
+com=player('Bot','bat')
+
+
+def switch(self, other=com):
+    self.role, other.role = other.role, self.role
+
+
