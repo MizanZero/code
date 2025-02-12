9559a91230b9e65102720e96cd3c8647d409043c
MizanZero <mizan.zafar2016@gmail.com>
2025-02-12 21:22:11 +0530

    first test

--- phy,scien,eng/damp spring oscillator.py
+++ phy,scien,eng/damp spring oscillator.py
@@ -1,7 +1,24 @@
-def x(t):
-    alpha1 = -b+sqrt(b**2 - omega**2)
-    alpha2 = -alpha1 - 2*b
+def x(t): return a1*exp(alpha1*t) + a2*exp(alpha2*t)
 
-    return a1*exp(alpha1*t) + a2*exp(alpha2*t)
+def v(t): return a1*alpha1*exp(alpha1*t) + a2*alpha2*exp(alpha2*t)
 
-def 
+t1 = int(input('Initial time: '))
+t2 = int(input('Final time: '))
+
+m = float(input('Mass: '))
+k = float(input('Spring constant: '))
+b = float(input('Damping constant: ')) /2
+omega = sqrt(k/m)
+
+num = int(input('Number of time points: '))
+
+alpha1 = -b+sqrt(b**2 - omega**2)
+alpha2 = -alpha1 - 2*b
+
+step = (t2-t1)/num
+
+t=t1
+
+for i in range(num):
+    t += step
+    print ("Time:",t, "Displacement:",x(t), "Velocity:",v(t))
