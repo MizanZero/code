3079a198d17857a0d955c612554d7e88e2d64f1c
MizanZero <mizan.zafar2016@gmail.com>
2025-04-15 21:32:27 +0530

    commit to the db

--- Web/Practice/mizan/database/sqlite/database.py
+++ Web/Practice/mizan/database/sqlite/database.py
@@ -2,3 +2,16 @@ import sqlite3
 
 db = sqlite3.connect('classroom.db')
 
+# cursor(instance) was made
+c = db.cursor()
+
+c.execute("""CREATE TABLE students(
+    first_name text,
+    last_name text,
+    roll_no text
+)""")
+
+db.commit() # commit changes to db
+
+db.close()
+
