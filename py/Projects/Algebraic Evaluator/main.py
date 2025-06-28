8ea418d384ff7a8df4ed94f3c22644c12df42b1e
MizanZero <mizan.zafar2016@gmail.com>
2025-06-28 21:55:04 +0530

    repr for const var ops only

--- py/Projects/Algebraic Evaluator/main.py
+++ py/Projects/Algebraic Evaluator/main.py
@@ -1,10 +1,37 @@
+"""
+private methods: _methName()
+"""
+
 class expression():
+    opList = '+-*/'
+
+
     def __init__(self, inp):
         self.inp = inp
+        self.formatted = self._format()
+        self.display = ' '.join(self.formatted)
 
-    def __repr__(self):
-        pass
+    def __str__(self) -> str:
+        return self.display
 
-exp = expression('x')
+    def _format(self) -> list: #private method
+        result = []
+        for char in self.inp:
+            if char in self.opList:
+                result.extend([' ', char, ' '])
+            elif char:
+                result.append(char)
+        return ''.join(result).split()
 
-print(exp.inp)
+    
+    class token():
+        def __init__(self, type:int, index:int):
+            self.type = type
+            self.index = index
+
+
+
+exp = expression('2x+ 4y*6')
+print(exp)
+print(exp.formatted)
+
