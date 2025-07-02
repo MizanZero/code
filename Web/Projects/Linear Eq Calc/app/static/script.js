32ea39a017dba94717c6ac5fdcbceed1604cce62
MizanZero <mizan.zafar2016@gmail.com>
2025-07-02 22:40:21 +0530

    finally lin eq calc test works

--- Web/Projects/Linear Eq Calc/app/static/script.js
+++ Web/Projects/Linear Eq Calc/app/static/script.js
@@ -3,11 +3,21 @@ const homeUrl = window.location.origin + '/start';
 console.log(homeUrl)
 console.log(window.location.href)
 
+function ping(){
+        fetch('/ping')
+                .then(res => {
+                        if (res.status === 440){
+                                window.location.href = '/offline';
+                        }
+                })
+}
+
 for (let input of document.querySelectorAll("input")){
         input.autocomplete = "off";
         input.inputMode = "numeric";
         input.pattern = "[0-9]*";
 }
 
-setInterval(() => fetch('/ping'), 10000);
+ping();
+setInterval(() => ping(), 5000);
 
